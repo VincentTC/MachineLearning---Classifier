a27d3f0e3fb78cefc7473a7a1c8ad4c56e2482f8
gerrykastogi <gerrykastogi@gmail.com>
2016-11-19 22:40:39 +0700

    MyJ48 update

---
+++ Classifier/src/J48/NotSplitable.java
@@ -0,0 +1,35 @@
+package J48;
+
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class NotSplitable extends NodeType{
+
+    public NotSplitable(J48ClassDistribution distribution)
+    {
+        classDistribution = new J48ClassDistribution(distribution);
+        numOfSubsets = 1;
+    }
+
+    @Override
+    public int getSubsetIndex(Instance instance) {
+        return 0;
+    }
+
+    @Override
+    public double[] getWeights(Instance instance) {
+        return null;
+    }
+
+    @Override
+    public final String leftSide(Instances instances){
+
+        return "";
+    }
+
+    @Override
+    public final String rightSide(int index, Instances instances){
+
+        return "";
+    }
+}
